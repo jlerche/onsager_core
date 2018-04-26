25ff39a192f5bca4597b2a215fe5e03c7d579a74
Jacob Lerche <jacob.lerche@gmail.com>
2018-04-25 22:28:50 -0700

    some work on util.ex

--- lib/onsager_core.ex
+++ lib/onsager_core.ex
@@ -1,2 +1,13 @@
 defmodule OnsagerCore do
+  @wait_print_interval 60 * 1000
+  @wait_poll_interval 100
+
+  def stop() do
+    stop("riak stop requested")
+  end
+
+  def stop(_reason) do
+    # lager:notice("~p", [reason])
+    :init.stop()
+  end
 end
---
+++ lib/onsager_core_util.ex
@@ -0,0 +1,62 @@
+defmodule OnsagerCore.Util do
+  @sec_to_epoch 62_167_219_200
+
+  def moment do
+    {mega, sec, _micro} = :os.timestamp()
+    mega * 1_000_000 + sec + @sec_to_epoch
+  end
+
+  def compare_dates(a = {_, _, _}, b = {_, _, _}) do
+    # assume 3-tuples are :erlang.now() times
+    a > b
+  end
+
+  def compare_dates(a, b) when is_list(a) do
+    #
+  end
+
+  defp rfc1123_to_now(string) when is_list(string) do
+    g_sec = :calendar.datetime_to_gregorian_seconds(:httpd_util.convert_request_date(string))
+    e_sec = g_sec - @sec_to_epoch
+    sec = rem(e_sec, 1_000_000)
+    m_sec = div(e_sec, 1_000_000)
+    {m_sec, sec, 0}
+  end
+
+  # replace_file
+  # read_file
+  # integer_to_list
+  # unique_id_62
+
+  def str_to_node(node) when is_atom(node) do
+    str_to_node(Atom.to_string(node))
+  end
+
+  def str_to_node(node_str) do
+    case String.split(node_str, "@") do
+      [node_name] ->
+        case node_hostname() do
+          [] ->
+            String.to_atom(node_name)
+
+          host_name ->
+            String.to_atom(node_name ++ "@" ++ host_name)
+        end
+
+      _ ->
+        String.to_atom(node_str)
+    end
+  end
+
+  defp node_hostname do
+    node_str = Atom.to_string(node())
+
+    case String.split(node_str, "@") do
+      [_node_name, host_name] ->
+        host_name
+
+      _ ->
+        []
+    end
+  end
+end
