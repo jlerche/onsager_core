08a2c56824278dd2aa5fecd62c8c98625769b642
Jacob Lerche <jacob.lerche@gmail.com>
2018-04-28 21:59:38 -0700

    adds vector clock module

--- lib/ring/ring.ex
+++ lib/ring/ring.ex
@@ -13,4 +13,30 @@ defmodule OnsagerCore.Ring do
     :seen,
     :rvsn
   ])
+
+  Record.defrecord(:meta_entry, [
+    :value,
+    :lastmod
+  ])
+
+  def set_tainted(ring) do
+    update_meta(:onsager_core_ring_tainted, true, ring)
+  end
+
+  def update_meta(key, val, state) do
+    change =
+      case Map.fetch(chstate(state, :meta), key) do
+        {:ok, old_meta} -> val / meta_entry(old_meta, :value)
+        :error -> true
+      end
+
+    case change do
+      true ->
+        state
+
+      _ ->
+        # TODO finish
+        meta = nil
+    end
+  end
 end
