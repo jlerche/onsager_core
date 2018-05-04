9c46b7759f11bf0020d901fbdb704478e00ef5f5
Jacob Lerche <jacob.lerche@hpe.com>
2018-05-04 10:26:07 -0700

    More work on ring

--- lib/ring/ring.ex
+++ lib/ring/ring.ex
@@ -1,11 +1,9 @@
 defmodule OnsagerCore.Ring do
   @moduledoc """
   Manages a node's local view of partition ownership. State is encapsulated
-  in the CHState struct (Consistent Hashing Struct) and exchanged between nodes
+  in the CHState struct (Consistent Hashing State struct) and exchanged between nodes
   via a gossip protocol.
   """
-
-  alias OnsagerCore.VectorClock, as: VC
 
   defmodule CHState do
     defstruct [
@@ -25,7 +23,7 @@ defmodule OnsagerCore.Ring do
             nodename: term,
             vclock: VC.vclock(),
             # change this after implementing chring
-            chring: term,
+            chring: OnsagerCore.CHash.chash(),
             meta: OnsagerCore.Ring.MetaEntry.t(),
             clustername: {term, term},
             next: [{integer, term, term, [module], :awaiting | :complete}],
@@ -43,6 +41,7 @@ defmodule OnsagerCore.Ring do
   end
 
   alias OnsagerCore.VectorClock, as: VC
+  alias OnsagerCore.CHash, as: CH
 
   @type member_status :: :joining | :valid | :invalid | :leaving | :exiting | :down
   @opaque onsager_core_ring :: CHState.t()
@@ -74,6 +73,7 @@ defmodule OnsagerCore.Ring do
     end
   end
 
+  @spec nearly_equal(chstate, chstate) :: boolean
   def nearly_equal(ring_a, ring_b) do
     test_vc = VC.descends(ring_b.vclock, ring_a.vclock)
     ring_a2 = %{ring_a | vclock: :undefined, meta: :undefined}
@@ -82,13 +82,53 @@ defmodule OnsagerCore.Ring do
     test_vc && test_ring
   end
 
+  @spec is_primary(chstate, {CH.index_as_int(), node}) :: boolean
   def is_primary(ring, idx_node) do
     owners = all_owners(ring)
     Enum.member?(owners, idx_node)
   end
 
+  @spec chash(chstate) :: CH.chash()
+  def chash(%CHState{chring: chash}) do
+    chash
+  end
+
+  def set_chash(state = %CHState{}, chash) do
+    %{state | chring: chash}
+  end
+
+  @doc """
+  Produces a list of all nodes that are members of the cluster
+  """
+  @spec all_members(chstate) :: [term]
+  def all_members(%CHState{members: members}) do
+    get_members(members)
+  end
+
+  def members(%CHState{members: members}, types) do
+    get_members(members, types)
+  end
+
+  @doc """
+  List of all active, as in not marked down, cluster members
+  """
+  def active_members(%CHState{members: members}) do
+    get_members(members, [:joining, :valid, :leaving, :exiting])
+  end
+
+  @doc """
+  List of members guaranteed safe for requests
+  """
+  def ready_members(%CHState{members: members}) do
+    get_members(members, [:valid, :leaving])
+  end
+
+  @doc """
+  Provide ownership information in the form of {index, node} pairs
+  """
+  @spec all_owners(chstate) :: [{integer, term}]
   def all_owners(state) do
-    []
+    CH.nodes(state.chring)
   end
 
   def get_meta(key, state) do
@@ -127,5 +167,13 @@ defmodule OnsagerCore.Ring do
       true ->
         state
     end
+  end
+
+  defp get_members(members) do
+    get_members(members, [:joining, :valid, :leaving, :exiting, :down])
+  end
+
+  defp get_members(members, types) do
+    for {node, {v, _, _}} <- members, Enum.member?(types, v), do: node
   end
 end
